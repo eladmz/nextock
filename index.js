2ca3a6e6fb465390125dcba9cfc45583b3fb49aa
eladmz <eladmz@gmail.com>
2023-07-11 02:27:17 +0300

    make it more awesome

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const { TELEGRAM_BOT_API_KEY, TELEGRAM_CHANNEL_ID, ITEMS_CSV_FILE_PATH } = proce
 const sentItems = new Set();
 
 const readItems = async () => {
+    const items = [];
+
     const data = await fs.readFile(ITEMS_CSV_FILE_PATH, 'utf-8');
     const lines = data.split('\n');
-
-    const items = [];
     for (const line of lines) {
         const parts = line.split(',');
         if (parts[0] && parts[1]) {
-            items.push({ url: parts[0], size: parts[1] });
+            items.push({ url: parts[0], value: parts[1] });
         }
     }
+
     return items;
 };
 
@@ -25,8 +26,10 @@ const extractItemIdFromUrl = (url) => {
     return `${fullId.substring(0, 3)}-${fullId.substring(3)}`;
 };
 
-const checkIfItemInStock = async (url, size) => {
-    let status = false;
+const checkIfItemInStock = async (url, value) => {
+    let inStock = false;
+    let size = 'N/A';
+    let price = 'N/A';
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
@@ -35,24 +38,33 @@ const checkIfItemInStock = async (url, size) => {
         .find('option')
         .each((i, option) => {
             const attributes = $(option).attr();
-            const stockSize = attributes['data-size'];
-            const stockStatus = attributes['data-stockstatus'];
-            if (stockSize === size && stockStatus === 'InStock') {
-                status = true;
+            const dataValue = attributes['value'];
+            const dataSize = attributes['data-size'];
+            const dataStockStatus = attributes['data-stockstatus'];
+            const dataPrice = attributes['data-price'];
+            if (dataValue === value && dataStockStatus === 'InStock') {
+                inStock = true;
+                size = dataSize;
+                price = dataPrice;
             }
         });
 
-    return status;
+    return { inStock, size, price };
 };
 
-const sendToTelegram = async (url, size) => {
+const sendToTelegram = async (url, size, price) => {
     console.log(`${url} in size ${size} was found!`);
     const requestBody = {
         chat_id: TELEGRAM_CHANNEL_ID,
         parse_mode: 'HTML',
-        text: `<b>Size: '${size}' is in Stock!</b>
-    
-${url}`
+        text: `<b>IN STOCK!</b>
+
+Size: <b>${size}</b>
+Price: <b>₪${price}</b>
+
+${url}
+
+<i>© Frrr Bots Inc. All rights reserved.</i>`
     };
     await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_API_KEY}/sendMessage`, requestBody);
 };
@@ -61,15 +73,15 @@ const main = async () => {
     try {
         const items = await readItems();
         for (const item of items) {
-            const { url, size } = item;
-            const hash = `${url}__${size}`;
+            const { url, value } = item;
+            const hash = `${url}__${value}`;
             if (sentItems.has(hash)) {
-                console.log(`${url} in size ${size} was already found! skipping...`);
+                console.log(`${url} with value ${value} was already found! skipping...`);
                 continue;
             }
-            const status = await checkIfItemInStock(url, size);
-            if (status) {
-                await sendToTelegram(url, size);
+            const { inStock, size, price } = await checkIfItemInStock(url, value);
+            if (inStock) {
+                await sendToTelegram(url, size, price);
                 sentItems.add(hash);
             }
         }
